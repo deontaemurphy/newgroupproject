4a90b68db3b933654f701775d9fb4d6919a2b746
Doctorine06 <fergtheartist@gmail.com>
2023-04-08 13:19:48 +0000

    //spaces

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,28 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import fontawesome from "font-awesome";
+// import fontawesome from "font-awesome";
 
 export const Navbar = () => {
   return (
     <nav className="navbar navbar-light bg-light">
-
-<li class="nav-item dropdown">
-            {/* <i> */}
-              className="fa-regular fa-bars fa-rotate-180" style="color:
-              #3c9c35;"
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Hamburger Menu
-          </a>
-   
+      <li class="nav-item dropdown">
+        {/* <i> */}
+        className="fa-regular fa-bars fa-rotate-180" style="color: #3c9c35;"
+        <a
+          class="nav-link dropdown-toggle"
+          href="#"
+          role="button"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          Hamburger Menu
+        </a>
+      </li>
       <div className="container">
         <Link to="/">
-          <button className="btn btn-primary">
+      <button className="btn btn-primary">
             {/* <i> */}
-              className="fa-regular fa-bars fa-rotate-180" style="color:
-              #3c9c35;"
+            className="fa-regular fa-bars fa-rotate-180" style="color: #3c9c35;"
             {/* </i> */}
             <span className="navbar-brand mb-0 h1">Navbar</span>
-          </button>
+      </button>
         </Link>
         <div className="ml-auto">
           <Link to="/demo">
