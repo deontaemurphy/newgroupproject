5cc81151a3eccb48b4bf8a5960f37df22809b2ed
Doctorine06 <fergtheartist@gmail.com>
2023-04-08 15:06:21 +0000

    git edit

--- src/api/routes.py
+++ src/api/routes.py
@@ -12,7 +12,6 @@ api = Blueprint('api', __name__)
 def handle_hello():
 
     response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+        "message": "Hello! I'm a message that came from the backend please check the network for a API Key"
 
     return jsonify(response_body), 200
