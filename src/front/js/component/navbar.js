627bae83b7dde472f6f11896e24a404645bfb92a
Doctorine06 <fergtheartist@gmail.com>
2023-04-08 13:01:39 +0000

    to main

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import fontawesome from "font-awesome";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container">
+        <Link to="/">
+          <button className="btn btn-primary">
+            {/* <i> */}
+              className="fa-regular fa-bars fa-rotate-180" style="color:
+              #3c9c35;"
+            {/* </i> */}
+            <span className="navbar-brand mb-0 h1">Navbar</span>
+          </button>
+        </Link>
+        <div className="ml-auto">
+          <Link to="/demo">
+            <button className="btn btn-primary">
+              Check the Context in action
+            </button>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
