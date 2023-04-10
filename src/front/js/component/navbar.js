8bafd391cc83f033efff234ff93036ee73d16d3b
Doctorine06 <fergtheartist@gmail.com>
2023-04-10 23:26:23 +0000

    modified Navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -5,23 +5,29 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
   return (
     <nav className="navbar navbar-light bg-light">
-      <div className="container">
+      <div id="mySidenav" className="sidenav">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">
+          &times;
+        </a>
+        <a href="https://3000-deontaemurp-newgrouppro-893lv3uwazv.ws-us93.gitpod.io/single/0">
+          About
+        </a>
+        {/* <div className="PlaceHolder"> */}
+
         <Link to="/">
-          <button className="btn btn-primary">
-            Navbar is in white?
-            <span className="navbar-brand mb-0 h1"></span>
-          </button>
+          {/* <button className="btn btn-primary">
+            DashBoard
+            <span className="navbar-brand mb-0 h1"></span> */}
+          {/* </button> */}
         </Link>
 
-            <div className="ml-auto">
-              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAzovqILuk-M9wwSilpOYPBdbsnRmiwBzM4g&usqp=CAU" />
+        <div className="ml-auto">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAzovqILuk-M9wwSilpOYPBdbsnRmiwBzM4g&usqp=CAU" />
         </div>
 
         <div className="ml-auto">
           <Link to="/demo">
-            <button className="btn btn-primary">
-             Make a Post
-            </button>
+            <button className="btn btn-primary">Make a Post</button>
           </Link>
         </div>
       </div>
