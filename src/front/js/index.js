3195cf60b0a3ea0d7a1f529ce672a221801edfa7
deontaemurphy <deontae2011@gmail.com>
2023-04-17 16:05:34 +0000

    added home page

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 
 //include your index.scss file into the bundle
 import "../styles/index.css";
-
+import "../styles/home.css";
 //import your own components
 import Layout from "./layout";
 
