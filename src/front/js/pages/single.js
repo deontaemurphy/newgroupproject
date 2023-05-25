ed7184438c1ddf103c22bae34745fe2417d9f135
Doctorine06 <fergtheartist@gmail.com>
2023-05-25 00:43:08 +0000

    dashboard

--- src/front/js/pages/single.js
+++ src/front/js/pages/single.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect, useContext } from "react";
 import PropTypes from "prop-types";
 import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 
 export const Single = (props) => {
   const { store, actions } = useContext(Context);
@@ -13,7 +12,7 @@ export const Single = (props) => {
       <h1 className="display-4">
         This will show the demo element: {store.demo[params.theid].title}
       </h1>
-      <img src={rigoImageUrl} />
+      <img src="https://wallpaperaccess.com/full/8011.jpg" />
       <hr className="my-4" />
 
       <Link to="/">
@@ -21,7 +20,6 @@ export const Single = (props) => {
           Back home
         </span>
       </Link>
-     
     </div>
   );
 };
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -21,68 +21,68 @@ const getState = ({ getStore, getActions, setStore }) => {
         getActions().changeColor(0, "green");
       },
       loadSomeData: () => {
-      changeColor: (index, color) => {
-        //get the store
-        const store = getStore();
+        changeColor: (index, color) => {
+          //get the store
+          const store = getStore();
 
-        //we have to loop the entire demo array to look for the respective index
-        //and change its color
-        const demo = store.demo.map((elm, i) => {
-          if (i === index) elm.background = color;
-          return elm;
-        });
+          //we have to loop the entire demo array to look for the respective index
+          //and change its color
+          const demo = store.demo.map((elm, i) => {
+            if (i === index) elm.background = color;
+            return elm;
+          });
 
-        //reset the global store
-        setStore({ demo: demo });
-      },
-  // const token = sessionStorage.getItem("token"),
-  // let cb_url = process.env.BACKEND_URL,
-  // return {
-  // store: {
-  // message: null,
+          //reset the global store
+          setStore({ demo: demo });
+        },
+          // const token = sessionStorage.getItem("token"),
+          // let cb_url = process.env.BACKEND_URL,
+          // return {
+          // store: {
+          // message: null,
 
-      //   evertime I come back must update cf_url by coping if from the browser of my frontend
-      // token: null,
-      // user: null,
-      // cf_url:
-        // "https:3000-deontaemurp-newgrouppro-gcgtzxginv1.ws-us93.gitpod.io/",
-      // username: null,
-    // },
-    // logout: () => {
-    // const cf_url = getStore().cf_url;
-    // const token = sessionStorage.removeItem("token");
-        setStore({ token: null });
+          //   evertime I come back must update cf_url by coping if from the browser of my frontend
+          // token: null,
+          // user: null,
+          // cf_url:
+          // "https:3000-deontaemurp-newgrouppro-gcgtzxginv1.ws-us93.gitpod.io/",
+          // username: null,
+          // },
+          // logout: () => {
+          // const cf_url = getStore().cf_url;
+          // const token = sessionStorage.removeItem("token");
+          setStore({ token: null });
         window.location.href = cf_url + "/";
       },
-        // login: async (email,password) => {
-        //   const opt = {
-        //     method: "POST",
-        //     headers: {
-        //       "Content-Type": "application/json",
-        //     },
-        //     body: JSON.stringify({
-        //       email: email,
-        //       password: password,
-        //       // user_id: user_id,
-        //     }),
-        //   };
-        //   const resp = await fetch(
-        //     "https://3001-deontaemurp-newgrouppro-8tf8x11ptk9.ws-us95.gitpod.io/admin/api/token",
-        //     opt
-        //   )
-        //   if (resp.status !== 200){
-        //     alert("there will be an error");
-        //     return false;
-        //   }
-        //   // const data = await resp.json();
-        //   //   .then((data) => {
-        //   //     console.log("this came from backend", data);
-        //   //     sessionStorage.setItem("token", data.access_token);
-        //   //   })
-        //   //   .catch((error) => {
-        //   //     console.error("there was an error", error);
-        //   //   })
-        // }
+      // login: async (email,password) => {
+      //   const opt = {
+      //     method: "POST",
+      //     headers: {
+      //       "Content-Type": "application/json",
+      //     },
+      //     body: JSON.stringify({
+      //       email: email,
+      //       password: password,
+      //       // user_id: user_id,
+      //     }),
+      //   };
+      //   const resp = await fetch(
+      //     "https://3001-deontaemurp-newgrouppro-8tf8x11ptk9.ws-us95.gitpod.io/admin/api/token",
+      //     opt
+      //   )
+      //   if (resp.status !== 200){
+      //     alert("there will be an error");
+      //     return false;
+      //   }
+      //   // const data = await resp.json();
+      //   //   .then((data) => {
+      //   //     console.log("this came from backend", data);
+      //   //     sessionStorage.setItem("token", data.access_token);
+      //   //   })
+      //   //   .catch((error) => {
+      //   //     console.error("there was an error", error);
+      //   //   })
+      // }
 
       login: async (email, password) => {
         const opts = {
@@ -162,14 +162,9 @@ const getState = ({ getStore, getActions, setStore }) => {
           // don't forget to return something, that is how the async resolves
           return data;
         } catch (error) {
-          console.log("Error loading message from backend", error);
+          console.log("Error from backend", error);
         }
       },
-
-
-      
-
-
 
       // changeColor: (index, color) => {
       //   //get the store
