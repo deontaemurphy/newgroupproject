424805fb50a8756fe59f68e6547de31d546c58ab
Doctorine06 <fergtheartist@gmail.com>
2023-07-25 17:24:32 +0000

    opo

--- src/front/js/component/Jumbotron1.js
+++ src/front/js/component/Jumbotron1.js
@@ -20,23 +20,22 @@ export const Jumbotron1 = () => {
         </a>
       </div>
       <div>
+        <iframe
+          width="1000"
+          height="400"
+          src="https://www.youtube-nocookie.com/embed/tme-KxDyiKY"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
         <div className="ml-auto">
           <Link to="/auth/search/thebookclub">
             <button className="btn btn-primary">Publish</button>
           </Link>
         </div>
         <img src="https://ia802607.us.archive.org/BookReader/BookReaderImages.php?zip=/4/items/adventuresofsher00doylrich/adventuresofsher00doylrich_jp2.zip&file=adventuresofsher00doylrich_jp2/adventuresofsher00doylrich_0023.jp2&id=adventuresofsher00doylrich&scale=4&rotate=0" />
-        <div>
-          <iframe
-            width="1000"
-            height="400"
-            src="https://www.youtube-nocookie.com/embed/tme-KxDyiKY"
-            title="YouTube video player"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen
-          ></iframe>
-        </div>
+        <div></div>
       </div>
     </nav>
   );
