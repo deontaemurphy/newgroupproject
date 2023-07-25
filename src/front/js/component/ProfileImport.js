3a517f593a3b38c0c96ea2d7f343ca727cbc1935
Doctorine06 <fergtheartist@gmail.com>
2023-07-25 18:13:22 +0000

    save

--- src/front/js/component/ProfileImport.js
+++ src/front/js/component/ProfileImport.js
@@ -2,6 +2,20 @@ import react from "react";
 
 const ProfileImport = () => {
   let Profile = "profile,setProfile";
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container">
+        <Link to="/searchthebookclub">
+          <span className="navbar-brand mb-0 h1">"Home"</span>
+        </Link>
+        <div className="ml-auto">
+          <Link to="/searchthebook">
+            <button className="btn btn-primary">Search</button>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default ProfileImport;
--- src/front/js/component/login.js
+++ src/front/js/component/login.js
@@ -1,13 +1,13 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-const Logout = () => {
-  const { store, actions } = useContext(Context);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const handleClick = (e) => {
-    e.preventDefault();
-    actions.login(email, password);
-  };
-};
+// import React, { Component } from "react";
+// import { Link } from "react-router-dom";
+// const Logout = () => {
+//   const { store, actions } = useContext(Context);
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const handleClick = (e) => {
+//     e.preventDefault();
+//     actions.login(email, password);
+//   };
+// };
 
-export default Logout;
+// export default Logout;
