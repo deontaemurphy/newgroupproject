4ac5ccea06ca59a88e6e769b7a5b9859a41947f1
Doctorine06 <fergtheartist@gmail.com>
2023-07-25 18:10:14 +0000

    searchAPI

--- src/front/js/pages/LoginComponent.js
+++ src/front/js/pages/LoginComponent.js
@@ -3,14 +3,27 @@ import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 import { useNavigate } from "react-router-dom";
 const Logout = () => {
-  const { store, actions } = useContext(Context);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const handleClick = (e) => {
-    e.preventDefault();
-    actions.login(email, password);
-  };
+    const { store, actions } = useContext(Context);
+    let navigate = useNavigate();
+    const handleClick = (e) => {
+      e.preventDefault();
+      actions.logout();
+      navigate("/");
+      actions.checkForToken();
 
-return()
+return(<nav className="navbar navbar-light bg-light">
+<div className="container">
+  <Link to="/searchthebookclub">
+    <span className="navbar-brand mb-0 h1">"Home"</span>
+  </Link>
+  <div className="ml-auto">
+    <Link to="/searchthebook">
+      <button className="btn btn-primary">Search</button>
+    </Link>
+  </div>
+</div>
+</nav>
+);
+};
 };
 export default Logout;
